06649bf0212c1695843feb8e589484af5b71ce00
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-05-23 00:30:33 +0600

    Update weekday.c

---
+++ weekday.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int wday;
+    printf("Enter the number 0-6:");
+    scanf("%d",&wday);
+    switch(wday){
+        case 0:
+        printf("Friday");
+        break;
+        case 1:
+        printf("Saturday");
+        break;
+        case 2:
+        printf("Sunday");
+        break;
+        case 3:
+        printf("Monday");
+        break;
+        case 4:
+        printf("Tuesday");
+        break;
+        case 5:
+        printf("Wednesday");
+        break;
+        case 6:
+        printf("Thursday");
+        break;
+        default:
+        printf("Invalid Weekday Number");
+    }
+    return 0;
+}
